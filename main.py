28715033446420bb4e131ca29eec64a7178d7f09
SWEETLINE <sharmaomprakash1993@gmail.com>
2025-10-22 20:47:52 +0530

    main

--- main.py
+++ main.py
@@ -1084,4 +1084,4 @@ def download_pdf(report_type,report_id):
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
